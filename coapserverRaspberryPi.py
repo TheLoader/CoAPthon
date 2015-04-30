3319460fc0b9c6c9b8fd4f148b324506cbfd13b3
Tanganelli <giacomo.tanganelli@for.unipi.it>
2015-04-30 13:04:00 +0200

    RaspBerry Pi

--- coapserverRaspberryPi.py
+++ coapserverRaspberryPi.py
@@ -1,3 +1,6 @@
+from coapthon import defines
+from coapthon.client.coap_synchronous import HelperClientSynchronous
+from coapthon.messages.request import Request
 from coapthon.resources.resource import Resource
 from twisted.internet import reactor
 from coapthon.server.coap_protocol import CoAP
@@ -5,25 +8,31 @@ from coapthon.server.coap_protocol import CoAP
 import RPi.GPIO as GPIO
 
 
+def callback(channel):
+    client = HelperClientSynchronous()
+    client.put({"path": "coap://127.0.0.1:5683/pin1", "payload": GPIO.input(channel)})
+
+
 class RPiResource(Resource):
     channel = 0
-    state = GPIO.low
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(channel, GPIO.IN)
+    GPIO.add_event_detect(channel, GPIO.RISING, callback=callback)
 
-    def __init__(self, name="RPiResource"):
-        super(RPiResource, self).__init__(name, visible=True,
-                                          observable=True, allow_children=True)
-        self.payload = "Basic Resource"
+    def render_GET(self, request):
+        self.payload = GPIO.input(self.channel)
+        return self
 
     def render_PUT(self, request):
-        GPIO.output(self.channel, self.state)
-        self.payload = self.state
+        self.payload = request.payload
         return self
 
 
 class CoAPServer(CoAP):
     def __init__(self, host, port, multicast=False):
         CoAP.__init__(self, multicast)
-        self.add_resource('temp/', RPiResource())
+        self.add_resource('temp/', RPiResource("channel0"))
+
         print "CoAP Server start on " + host + ":" + str(port)
         print(self.root.dump())
 
